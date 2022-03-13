429c44b008e49a7873d81722a70943302cd7e428
Theo Lemay <16546293+theol0403@users.noreply.github.com>
2022-03-13 17:16:04 -0400

    Delete display_image.py

--- display_image.py
+++
@@ -1,7 +0,0 @@
-from keras.models import Sequential, Dense
-
-model = Sequential()
-model.add(Dense(16, input_dim=28 * 28, activation="relu"))
-model.add(Dense(10, activation="relu"))
-model.compile(optimizer="sgd", loss="mse", metrics=["accuracy"])
-
